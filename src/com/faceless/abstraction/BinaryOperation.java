ccdd1a64fddef1c4db5a433cd05d0bf58993d95b
FacelessLord <skyres21@yandex.ru>
2020-03-22 15:01:28 +0500

    Operation Api Expansion

--- src/com/faceless/abstraction/BinaryOperation.java
+++ src/com/faceless/abstraction/BinaryOperation.java
@@ -2,10 +2,25 @@ package com.faceless.abstraction;
 
 public abstract class BinaryOperation<T> implements IOperation<T> {
     @Override
+    public String getExpressionString(String... args) {
+        return null;
+    }
+
+    @Override
+    public String getOperationSymbol() {
+        return null;
+    }
+
+    @Override
+    public int getOperationPriority() {
+        return 0;
+    }
+
+    @Override
     public int getArgumentCount() {
         return 2;
     }
 
     @Override
-    public abstract T apply(T[] args);
+    public abstract T apply(T... args);
 }
